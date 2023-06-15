2552b973479790c9ba1a6b19c45ae3728f03be69
Vihan Agarwal <vihanagarwal2003@gmail.com>
2023-06-15 17:49:22 +0530

    initial

---
+++ cppTestReInstall.cpp
@@ -0,0 +1,8 @@
+#include<iostream >
+
+
+int main()
+{
+    std::cout << "lesgo1 World!" << std::endl;
+    return 0;
+}
---
+++ flutterTestReInstall/test_re_install/android/app/src/main/kotlin/com/example/test_re_install/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_re_install
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
